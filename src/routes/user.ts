44233927a53744dc7fcb5012d7cc4d8c6cce8249
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2024-09-20 08:45:23 -0300

    feat: add users migration and register route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,29 @@
-import { FastifyInstance } from "fastify";
+import { knex } from '../database'
+import { z } from 'zod'
+import bcrypt from 'bcryptjs'
+import { randomUUID } from 'node:crypto'
+import { FastifyInstance } from 'fastify'
 
 export async function userRoutes(app: FastifyInstance) {
-  app.post("/", async () => {});
+  app.post('/register', async (request, reply) => {
+    console.log('body', request.body)
+    const userSchema = z.object({
+      fullName: z.string(),
+      email: z.string(),
+      password: z.string(),
+    })
+
+    const { fullName, email, password } = userSchema.parse(request.body)
+
+    const hashedPassword = bcrypt.hash(password, 5)
+
+    await knex('users').insert({
+      id: randomUUID(),
+      fullName,
+      email,
+      password: hashedPassword,
+    })
+
+    return reply.status(201).send()
+  })
 }
